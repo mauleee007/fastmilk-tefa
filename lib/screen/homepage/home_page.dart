9960dc79ca33f1216c4f8fc05734c7b30a0592c3
adriansatria <adriansatria.siswa@smkn4bdg.sch.id>
2021-04-23 16:33:40 +0700

    Read data

--- lib/screen/homepage/home_page.dart
+++ lib/screen/homepage/home_page.dart
@@ -1,7 +1,12 @@
 import 'package:fastmilk_admin/screen/homepage/component/body_homepage.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(HomePage());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(HomePage());
+}
 
 class HomePage extends StatelessWidget {
   static String tag = 'home-page';
--- lib/screen/list_pegawai/data_pegawai.dart
+++ lib/screen/list_pegawai/data_pegawai.dart
@@ -1,5 +1,14 @@
 import 'package:fastmilk_admin/screen/list_pegawai/component/body_datapegawai.dart';
+import 'package:fastmilk_admin/screen/list_pegawai/component/bodydata.dart';
+import 'package:fastmilk_admin/screen/tambah_data/tambah_datapegawai.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+
+void main() async {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp();
+  runApp(DataPegawai());
+}
 
 class DataPegawai extends StatelessWidget {
   @override
@@ -7,6 +16,19 @@ class DataPegawai extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) => TambahPegawai()),
+              );
+            },
+            backgroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                side: BorderSide(color: Color(0xFFFE931D)),
+                borderRadius: BorderRadius.all(Radius.circular(30))),
+            child: Icon(Icons.add, color: Color(0xFFFE931D)),
+          ),
           appBar: AppBar(
             title: Text('DATA PEGAWAI',
                 style: TextStyle(
