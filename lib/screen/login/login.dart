1ed5fb10389d34334b1ab68f57f7aa453b7fd199
Mauleee007 <adrianfirdaus098@gmail.com>
2021-04-26 08:29:58 +0700

    logout

--- lib/screen/login/login.dart
+++ lib/screen/login/login.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 import 'component/body.dart';
 
 class Login extends StatelessWidget {
-  static String routeName = '/login';
+  static String routeName = "/login";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
